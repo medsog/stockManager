6f4be5851dcdd067533ddacbc0846b73d3705e7c
medsog <mohamedsoghri@gmail.com>
2017-05-01 15:28:22 +0100

    model entete

---
+++ magasin-app/src/app/Models/Entete.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by medsog on 01/05/2017.
+ */
+export class Entete {
+//la representation js de la table entete
+    id:number;
+    n_piece:string = '';
+    date_piece:string;
+    id_depot:number;
+    reference:string = '';
+    id_affaire:number;
+    type:number;
+    id_client:number;
+    id_collaborateur:number;
+    date_livraison:string = '0';
+    mode_payement:number;
+
+}
